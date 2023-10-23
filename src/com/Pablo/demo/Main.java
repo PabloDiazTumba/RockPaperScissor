5f3d5e715ead0debcc47fed0e9cbacfefadcfa2d
PabloDiazTumba <pablodiaz147@gmail.com>
2023-10-23 22:53:20 +0200

    First commit

--- src/com/Pablo/demo/Main.java
+++ src/com/Pablo/demo/Main.java
@@ -10,8 +10,28 @@ public class Main {
         String computerMove = rps[new Random().nextInt(rps.length)];
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your move (R, P, or S)");
-        String playerMove = scanner.nextLine();
+        String playerMove;
+
+        while (true) {
+            System.out.println("Enter your move (R, P, or S)");
+            playerMove = scanner.nextLine();
+            if (playerMove.equals("R") || playerMove.equals("p") || playerMove.equals("S")) {
+                break;
+            }
+            System.out.println(playerMove + " is not a valid move");
+        }
+
+        System.out.println("Computer played " + computerMove);
+        if (playerMove.equals(computerMove)) {
+            System.out.println("The game was a tie!");
+        } else if (playerMove.equals("R")) {
+            if (computerMove.equals("P")) {
+                System.out.println("You lose!");
+            } else if (computerMove.equals("S")) {
+                System.out.println("You win!");
+
+            }
+        }
 
     }
 }
